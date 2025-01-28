531c0382c5216d0ebf031710801d9edffd7ad27a
Federico Capaldo <federico.capaldo@hotmail.com>
2025-01-28 08:14:15 +0000

    added stimulus controller to do something turbo can t, updating which is the active contact in the list by changing its background color when clicking

---
+++ app/javascript/controllers/contacts_list_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="contacts-list"
+export default class extends Controller {
+  static targets = ["contact"]
+
+  connect() {
+    console.log(this.contactTargets)
+  }
+
+  highlight(event) {
+    this.contactTargets.forEach((singleContact) => {
+      singleContact.classList.remove('active')
+    })
+    event.currentTarget.classList.add('active')
+  }
+}
